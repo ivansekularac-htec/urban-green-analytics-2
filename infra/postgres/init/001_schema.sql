8b199c199c4236c64a16c3ef015ab665d82130ec
Vladimir Popovic <vladimir.popovic@htecgroup.com>
2026-06-09 11:34:17 +0200

    Fix typo for updated_at index

--- infra/postgres/init/001_schema.sql
+++ infra/postgres/init/001_schema.sql
@@ -192,6 +192,6 @@ CREATE INDEX idx_harvests_farm_id
 CREATE INDEX idx_harvests_crop_id
     ON harvests(crop_id);
 
-CREATE INDEX idx_harvests_created_at
+CREATE INDEX idx_harvests_updated_at
     ON harvests(updated_at);
 
