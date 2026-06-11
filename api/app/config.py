e075ba5a20d19b572af8094582b0fc7722163912
Stefan Mikic <stefan.mikic@htecgroup.com>
2026-06-11 13:14:57 +0200

    final changes after pr review

--- api/app/config.py
+++ api/app/config.py
@@ -1,24 +1,22 @@
-from functools import cached_property
-
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    POSTGRES_HOST: str = Field(..., description="Database host")
-    POSTGRES_PORT: int = Field(5432, description="Database port")
-    POSTGRES_USER: str = Field(..., description="Database user")
-    POSTGRES_PASSWORD: str = Field(..., description="Database password")
-    POSTGRES_DB: str = Field(..., description="Database name")
+    postgres_host: str = Field(..., description="Database host")
+    postgres_port: int = Field(5432, description="Database port")
+    postgres_user: str = Field(..., description="Database user")
+    postgres_password: str = Field(..., description="Database password")
+    postgres_db: str = Field(..., description="Database name")
 
-    @cached_property
+    @property
     def database_url(self) -> str:
         return (
-            f"postgresql+psycopg://{self.POSTGRES_USER}:"
-            f"{self.POSTGRES_PASSWORD}@"
-            f"{self.POSTGRES_HOST}:"
-            f"{self.POSTGRES_PORT}/"
-            f"{self.POSTGRES_DB}"
+            f"postgresql+psycopg://{self.postgres_user}:"
+            f"{self.postgres_password}@"
+            f"{self.postgres_host}:"
+            f"{self.postgres_port}/"
+            f"{self.postgres_db}"
         )
 
     model_config = SettingsConfigDict(
