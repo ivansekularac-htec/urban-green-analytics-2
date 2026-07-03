ed9bbe8072a305dbae357972536b9034e63aac64
marijahtec <marija.bjelanovic@htecgroup.com>
2026-07-03 14:06:53 +0200

    Create sensor_readings Topic via Init Container (#123)

---
+++ infra/kafka/init/init-topics.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -eu
+
+BOOTSTRAP_SERVER="urbangreen-kafka:${KAFKA_PORT_INTERNAL:-9092}"
+TOPIC_NAME="${KAFKA_TOPIC_SENSOR_READINGS:-sensor_readings}"
+
+echo "Creating Kafka topic if it does not exist..."
+echo "Bootstrap server: ${BOOTSTRAP_SERVER}"
+echo "Topic: ${TOPIC_NAME}"
+
+/opt/kafka/bin/kafka-topics.sh \
+  --bootstrap-server "${BOOTSTRAP_SERVER}" \
+  --create \
+  --if-not-exists \
+  --topic "${TOPIC_NAME}" \
+  --partitions 3 \
+  --replication-factor 1
+
+echo "Kafka topic initialization completed."
