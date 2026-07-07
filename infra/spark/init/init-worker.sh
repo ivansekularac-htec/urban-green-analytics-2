cd74fe87f7c68c314610f87c868cbbdf248bc958
Marija Bjelanovic <marija.bjelanovic@htecgroup.com>
2026-07-07 10:59:18 +0200

    Run Two Spark Worker Containers and fix CRLF line endings

---
+++ infra/spark/init/init-worker.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -eu
+
+exec /opt/spark/bin/spark-class org.apache.spark.deploy.worker.Worker \
+  "spark://urbangreen-spark-master:${SPARK_MASTER_PORT:-7077}" \
+  --cores "${SPARK_WORKER_CORES:-1}" \
+  --memory "${SPARK_WORKER_MEMORY:-1g}"
